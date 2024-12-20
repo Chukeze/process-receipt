aa68802d4f394fcc5a7b29a1372094982a10eb0c
Chukeze <enile12@gmail.com>
2024-12-19 22:29:37 -0600

    Update Database Schema

---
+++ models/receipt.go
@@ -0,0 +1,22 @@
+package models
+
+import (
+	"errors"
+	"regexp"
+	"time"
+)
+
+type Item struct {
+	ShortDescription string `json:"shortDescription"`
+	Price string `json:"price"`
+}
+
+type Receipt struct {
+	Retailer string `json:"retailer"`
+	PurchaseDate string `json:"purchaseDate"`
+	PurchaseTime string `json:"purchaseTime"`
+	Items []Item `json:"items"`
+	Total string `json:"total"`
+}
+
+

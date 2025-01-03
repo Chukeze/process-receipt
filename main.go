2c72a8a643971b2f29e48a7095df0acfdd669222
Chukeze <enile12@gmail.com>
2025-01-02 19:42:01 -0600

    Fix route

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	router := mux.NewRouter();
 
 	router.HandleFunc("/receipts/process", handlers.ProcessReceipt).Methods("POST");
-	router.HandleFunc("receipts/{id}/points", handlers.GetPoints).Methods("GET");
+	router.HandleFunc("/receipts/{id}/points", handlers.GetPoints).Methods("GET");
 
 	log.Println("Server started on port 8080");
 	log.Fatal(http.ListenAndServe(":8080", router));
